abf2f581a7699cfa6b8cf05e72be524c1f8972f7
LS-softwareEngineer <luke.steward01@gmail.com>
2026-09-14 11:40:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
   return {
     base: process.env.FIGMA_PUBLIC_URL
   ? `${process.env.FIGMA_PUBLIC_URL}/`
-  : '/Creative-Source-Agency/',
+  : '/CreativeSourceAgency/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
